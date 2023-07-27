fe0167e9f8388484aa8b616cafa8143512941af8
dkdl2095 <jo2095@naver.com>
2023-07-27 16:01:23 +0900

    2023-07-27 16:01

--- Project/src/main/java/dbsql/Complet.java
+++ Project/src/main/java/dbsql/Complet.java
@@ -23,7 +23,7 @@ public class Complet extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.setContentType("text/html; charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
 		String action = request.getParameter("action");
 		String view = "";
 		if (action == null) {
